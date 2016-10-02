ad0c615dbf00f1f32735954b4c4d9d465fde12ad
PC\Admin <thangtq92@gmail.com>
2016-10-02 22:37:25 +0700

    the first Model

---
+++ Tedu.Model/Models/Footer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tedu.Model.Models
+{
+    [Table("Footers")]
+    internal class Footer
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
