734c0264da407f09fbfdb44de0fb8bf743c3d5a0
PC\Admin <thangtq92@gmail.com>
2016-10-09 22:08:09 +0700

    How to use Web API

---
+++ Tedu.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Tedu.Data.Infrastructure;
+using Tedu.Data.Repositories;
+using Tedu.Model.Models;
+
+namespace Tedu.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        private IDbFactory dbFactory;
+        private IPostCategoryRepository objRepository;
+        private IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+            Assert.AreEqual(5, list.Count);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory
+            {
+                Name = "Test category",
+                Alias = "test-category",
+                Status = true
+            };
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(5, result.ID);
+        }
+    }
+}
