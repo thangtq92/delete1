865079c136746f908b468dfe0da291f8b18f69bf
PC\Admin <thangtq92@gmail.com>
2016-10-09 09:53:52 +0700

    Commit PostCategoryService

---
+++ Tedu.Service/PostCategoryService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Tedu.Data.Infrastructure;
+using Tedu.Data.Repositories;
+using Tedu.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IPostCategoryService
+    {
+        PostCategory Add(PostCategory postCategory);
+
+        void Update(PostCategory postCategory);
+
+        PostCategory Delete(int id);
+
+        IEnumerable<PostCategory> GetAll();
+
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+
+        PostCategory GetById(int id);
+
+        void Save();
+    }
+
+    public class PostCategoryService : IPostCategoryService
+    {
+        private IPostCategoryRepository _postCategoryRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        PostCategory IPostCategoryService.Add(PostCategory postCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        PostCategory IPostCategoryService.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
